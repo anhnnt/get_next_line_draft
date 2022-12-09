4e5f34abd6dc6e72c8847261272e254baad8c1aa
Anh Nguyen <33486007+anhnnt@users.noreply.github.com>
2022-12-09 18:08:38 +0200

    test main.c

--- gnl_draft/main.c
+++ gnl_draft/main.c
@@ -12,16 +12,36 @@
 
 #include "get_next_line.h"
 #include <fcntl.h>
-int	main()
-{
-	int		fd;
-	char	*line;
+// int	main()
+// {
+// 	int		fd;
+// 	char	*line;
 
-	fd = open("text", O_RDONLY);
-	while (line = get_next_line(fd))
+// 	fd = open("text", O_RDONLY);
+// 	line = NULL;
+// 	while (line == get_next_line(fd))
+// 	{
+// 		printf("%s\n", line);
+// 		free(line);
+// 	}
+// 	return (0);
+// }
+
+int main()
+{
+	int	fd;
+	int	ret;
+	char	*buf;
+
+	fd = open("text.txt", O_RDONLY);
+	buf = (char *)malloc(sizeof(char) * (5 + 1));
+	if (!buf)
+		return (NULL);
+	while (ret != '\0')
 	{
-		printf("%s\n", line);
-		free(line);
+	ret = read(fd, buf, 6);
+	buf[ret] = '\0';
+	printf("%s", buf);
 	}
 	return (0);
 }
